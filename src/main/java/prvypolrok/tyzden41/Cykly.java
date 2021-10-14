d48397c81fca8cc5d6e26409dae776d35acaf522
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2021-10-14 10:28:52 +0200

    .

--- src/main/java/prvypolrok/tyzden41/Cykly.java
+++ src/main/java/prvypolrok/tyzden41/Cykly.java
@@ -4,12 +4,32 @@ public class Cykly {
 
     public static void main(String[] args) {
 
+        vypisanie10Cisiel();
+
         //inicializacia pola typu int a naplnenie desiatimi cislami
+    }
+
+    static void cyklusApole(){
+
         int[] poleCisel = new int[]{1,2,3,4,5,6,7,8,9,10};
 
         int nultyindex =poleCisel[0];
         //vypisanie dlzky pola
         System.out.println(poleCisel.length);
+        for (int i = 0; i < poleCisel.length; i++){
+            System.out.println(poleCisel[i]);
+        }
+    }
 
+    static void vypisanie10Cisiel(){
+        int x=0;
+        int vysledok = 0;
+        for (int i = 1; i <= 10; i++) {
+           //System.out.println("pred ulozenim: " +x);
+            //x=i;
+            //System.out.println("po ulozeni: " + x);
+            vysledok = vysledok + i;
+        }
+        System.out.println(vysledok);
     }
 }
