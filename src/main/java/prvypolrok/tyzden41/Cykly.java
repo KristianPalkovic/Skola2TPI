424b9f2fdfefddb998419c8adcdf7d071f61f81b
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2021-10-14 11:30:17 +0200

    .

--- src/main/java/prvypolrok/tyzden41/Cykly.java
+++ src/main/java/prvypolrok/tyzden41/Cykly.java
@@ -5,6 +5,7 @@ public class Cykly {
     public static void main(String[] args) {
 
         vypisanie10Cisiel();
+        prepisanieNaWhile();
 
         //inicializacia pola typu int a naplnenie desiatimi cislami
     }
@@ -25,11 +26,21 @@ public class Cykly {
         int x=0;
         int vysledok = 0;
         for (int i = 1; i <= 10; i++) {
-           //System.out.println("pred ulozenim: " +x);
-            //x=i;
-            //System.out.println("po ulozeni: " + x);
+           System.out.println("pred ulozenim: " +x);
+            x=i;
+            System.out.println("po ulozeni: " + x);
             vysledok = vysledok + i;
         }
         System.out.println(vysledok);
     }
+
+     static void prepisanieNaWhile(){
+
+        int i = 1;
+        while(i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
 }
+
