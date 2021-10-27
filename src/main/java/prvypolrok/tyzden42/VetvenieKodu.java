768692713d1e4d97eaa895697797734d2f32ac11
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2021-10-27 10:26:27 +0200

    .

--- src/main/java/prvypolrok/tyzden42/VetvenieKodu.java
+++ src/main/java/prvypolrok/tyzden42/VetvenieKodu.java
@@ -16,4 +16,26 @@ public class VetvenieKodu {
             System.out.println("moje meno je mensie ako 15 znakov");
         }
     }
+
+    public static void lenIf(boolean vieJavu, String meno){
+
+        if(vieJavu){
+            meno = meno + " vie Javu";
+        }
+
+        meno = meno + " vie matematiku";
+
+        System.out.println(meno);
+    }
+
+    public static void lenElse(boolean vieJavu, String meno) {
+
+        if (vieJavu) {
+            meno = meno + " vie Javu";
+        }
+
+        meno = meno + " vie matematiku";
+
+        System.out.println(meno);
+    }
 }
