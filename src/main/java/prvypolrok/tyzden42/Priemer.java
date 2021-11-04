59d81d6f44f30485a9b89139f226650ba6387c60
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2021-11-04 10:22:21 +0100

    .

---
+++ src/main/java/prvypolrok/tyzden42/Priemer.java
@@ -0,0 +1,17 @@
+package prvypolrok.tyzden42;
+
+public class Priemer {
+
+    public static void main(String[] args) {
+        int[] pole = new int[]{1,46,4,34,5,76,44,34,5,6,6,77};
+        priemer(pole);
+
+    }
+
+    static double priemer(int[] poleCisiel){
+        // tu ma byt riesenie
+        // pomocka priemer vypocitanie sucet clenov/pocet
+
+
+    }
+}
