51a1cfa160a85429b6367ad878d97513b626d122
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2021-11-11 11:30:51 +0100

    .

---
+++ src/main/java/prvypolrok/Ulohy/macka.java
@@ -0,0 +1,23 @@
+package prvypolrok.Ulohy;
+
+public class macka {
+
+    public static boolean main(String[] args) {
+
+       public static boolean isCatPlaying(boolean leto,int teplota){
+
+            if(leto){
+                if(){
+
+
+                }
+
+            }else{
+
+
+            }
+
+            return false;
+        }
+    }
+}
