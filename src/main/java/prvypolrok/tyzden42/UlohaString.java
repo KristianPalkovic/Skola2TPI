b1d21eca743f6a29b9b8aaf5b7d50e126a7279f3
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2021-11-11 10:10:52 +0100

    .

---
+++ src/main/java/prvypolrok/tyzden42/UlohaString.java
@@ -0,0 +1,52 @@
+package prvypolrok.tyzden42;
+
+import java.util.Arrays;
+
+public class UlohaString {
+
+    public static void main(String[] args) {
+         test();
+         osemPoli();
+    }
+    static String text = " Clovek mierou svojeho chápania pracuje na seba, " + "a mierou nechápania na tých, kto chápe viac";
+
+    static void test(){
+
+            char[] poleZnakov = text.toCharArray();
+int pocet = 0;
+        for(int i=0; i < text.length(); i++){
+            if(text.charAt(i) ==' '){
+                pocet = pocet +1;
+            }
+
+        }
+    }
+
+    static void osemPoli(){
+
+        char p = 'p';
+        int nieco = p + 1;
+        int pecko = nieco -1;
+
+        System.out.println(p + " " + pecko);
+
+        char[] test = new char[4];
+
+        //int [] x = new int[s];
+        //check[] y = {0,8,10,10};
+        //char u = y[0];
+        //y[0] = 'Z';
+        //java.util.Arrays;
+
+        int [] test1 = new int[] {2,5,66,77,898,34,2433,5676,367,-653};
+        int vysledok1= Arrays.binarySearch(test1,367);
+        System.out.println(vysledok1);
+
+        Arrays.sort(test1);
+        vysledok1=Arrays.binarySearch(test1,367);
+        System.out.println(vysledok1);
+
+        System.out.println(Arrays.toString(test1));
+    }
+
+}
