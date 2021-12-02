d41fd4e25c2be9549daf96bd1d7ad1b46d68521e
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2021-12-02 10:22:35 +0100

    .

---
+++ src/main/java/prvypolrok/tyzden47/uloha3.java
@@ -0,0 +1,19 @@
+package prvypolrok.tyzden47;
+
+public class uloha3 {
+
+    public static void main(String[] args) {
+        int retazec = 13;
+    }
+    static String vratiString(String x){
+        String uprava = x +"01";
+        return uprava;
+    }
+    static int vratiInt(String x){
+        String uprava = x +"01";
+        return uprava.length();
+    }
+}
+
+//uloha: vytvorte si triedu s nazvom tyzden49 a do triedy vytvorte metodu s nazvom vratMiCislo ktora vrati cislo a
+//bude prijimat jeden parameter a to pole. chcem aby metoda vratila pocet elementov dannom poli
