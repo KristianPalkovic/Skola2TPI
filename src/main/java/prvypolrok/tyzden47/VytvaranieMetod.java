1e9ca07036a24155da8ba5b0da93dd12008b8a70
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2021-12-16 10:29:01 +0100

    .

--- src/main/java/prvypolrok/tyzden47/VytvaranieMetod.java
+++ src/main/java/prvypolrok/tyzden47/VytvaranieMetod.java
@@ -25,7 +25,7 @@ public class VytvaranieMetod {
     static void metoda6(boolean[]d , int x){
 
     }
-    static boolean metoda7(String x , char z , long f ){
+    static boolean metoda7(String a , char b , long c ){
         return true;
     }
     static float metoda8(float x){
@@ -33,11 +33,46 @@ public class VytvaranieMetod {
     }
     static boolean[] metoda9(){
 
-        boolean a[] =  {true, false, true, false};
-        return a;
+        boolean v[] =  {true, false, true, false};
+        return v;
 
     }
-    static void metoda10(boolean z ){
+    static void metoda10(boolean l ){
 
     }
+
+    static void premenne(){
+        int a = 4;
+        boolean u = true;
+        long l =456789123L;
+        String c = "text";
+        float x =  4578.47f;
+        double d = 12.5;
+        char ch = 'f';
+    }
+    static void polia(){
+        String[] x = new String[] {"dva","tri","štyri","päť","jeden"};
+        char[] chars = new char[] {'f'};
+        boolean [] g = new boolean[] {true,false,true,false,true};
+        float [] floats = new float[] {12.545f,12.545f,12.545f,12.545f,12.545f};
+        long [] longs = new long[] {15165L,15165L,15165L,24890L,47654L};
+
+        for(int u=0;u<x.length ;u++){
+            System.out.println(x[u]);
+        }
+        for(int l=0;l<chars.length ;l++){
+            System.out.println(chars[l]);
+        }
+        for(int u=0;u<x.length ;u++){
+            System.out.println(g[u]);
+        }
+        for(int j=0;j<floats.length ;j++){
+            System.out.println(floats[j]);
+        }
+        for(int z=0;z<longs.length ;z++){
+            System.out.println(g[z]);
+        }
+    }
+
+
 }
