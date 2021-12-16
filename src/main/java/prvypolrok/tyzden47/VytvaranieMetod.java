74e4e07d497b7176613af01a93aed3a6e27a104b
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2021-12-16 08:53:34 +0100

    .

---
+++ src/main/java/prvypolrok/tyzden47/VytvaranieMetod.java
@@ -0,0 +1,43 @@
+package prvypolrok.tyzden47;
+
+public class VytvaranieMetod {
+
+    static void metoda1(int x){
+
+    }
+    static String metoda2(int y , String c){
+
+        return "text";
+    }
+    static String metoda3(boolean u , String j){
+
+        return "nejakyText";
+    }
+    static long metoda4(){
+
+        return 789456213;
+    }
+    static String[] metoda5(int[] nieco){
+
+        String[] x = new String[] {"text","text2","text3"};
+        return x;
+    }
+    static void metoda6(boolean[]d , int x){
+
+    }
+    static boolean metoda7(String x , char z , long f ){
+        return true;
+    }
+    static float metoda8(float x){
+        return 4587;
+    }
+    static boolean[] metoda9(){
+
+        boolean a[] =  {true, false, true, false};
+        return a;
+
+    }
+    static void metoda10(boolean z ){
+
+    }
+}
