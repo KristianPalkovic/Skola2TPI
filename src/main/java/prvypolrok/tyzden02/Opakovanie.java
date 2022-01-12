2de5607a74c54a963a24c71874f56de316f7f23a
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2022-01-12 10:27:19 +0100

    .

---
+++ src/main/java/prvypolrok/tyzden02/Opakovanie.java
@@ -0,0 +1,14 @@
+package prvypolrok.tyzden02;
+
+public class Opakovanie {
+    public static void main(String[] args) {
+
+        long velkeCislo = 3_000_000_000L; //64b
+        int x = 5; // 32b
+        short y = 7; // 16b
+        byte z = 127; // 8b
+        float f = 5.45f;
+        double d = 5.43;
+
+    }
+}
