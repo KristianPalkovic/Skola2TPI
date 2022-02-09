8f7460273320648dfa39b74c673e42531afc6928
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2022-02-09 10:27:54 +0100

    .

--- src/main/java/druhypolrok/tyzden06/UlohaArrayList02.java
+++ src/main/java/druhypolrok/tyzden06/UlohaArrayList02.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class UlohaArrayList02 {
-
+ static ArrayList<Integer> pole = new ArrayList<>();
     public static void main(String[] args) {
-        ArrayList<Integer> pole = new ArrayList<>();
+
 
         pridanieDoPola(3);
         pridanieDoPola(4);
@@ -23,12 +23,20 @@ public class UlohaArrayList02 {
 
     private static void zoradeniePola(){
         //zoradenie pola pouzi metodu sort
-
     }
 
     private static void pridanieDoPola(int i){
         //pridanie do pola len vtedy ak je element unikatny
         //ak je unikatny prida sa do pola a do konzoly vypise: element X pridany
         //ak nie je  unikatny tak sa len vypise do konzoly: element X uz existuje v poli
+
+        if(pole.contains(i)){
+
+            System.out.println("element " + "už existuje v poli");
+        }else{
+            pole.add();
+            System.out.println("element " + "pridaný");
+
+        }
     }
 }
