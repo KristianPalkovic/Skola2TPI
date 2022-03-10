7f86b8aacc4b64c935334ad44e1ff909d65cd978
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2022-03-10 10:28:11 +0100

    .

---
+++ src/main/java/druhypolrok/tyzden10/TestovaciaTrieda.java
@@ -0,0 +1,29 @@
+package druhypolrok.tyzden10;
+
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.ArrayList;
+
+public class TestovaciaTrieda {
+
+    public static void main(String[] args) {
+
+        Osoba defaultnaOsoba = new Osoba();
+
+        Osoba jedna = new Osoba("Pišta", "Ignác", 150 );
+        Osoba dva = new Osoba("Fero", "Blažek", 160 );
+        Osoba tri = new Osoba("Gregor", "Zelený", 170 );
+
+        ArrayList<Osoba> zoznamOsob = new ArrayList<>();
+        zoznamOsob.add(defaultnaOsoba);
+        zoznamOsob.add(jedna);
+        zoznamOsob.add(dva);
+        zoznamOsob.add(tri);
+        zoznamOsob.add(new Osoba());
+
+        for (Osoba osobaVCykle : zoznamOsob){
+            System.out.println(osobaVCykle.uciSaJavu());
+        }
+        //System.out.println(defaultnaOsoba);
+
+    }
+}
