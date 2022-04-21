4994abd4e2994b908e7f32aa0ecb6308a8533951
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2022-04-21 09:51:47 +0200

    ss

--- src/main/java/druhypolrok/tyzden16/Muz.java
+++ src/main/java/druhypolrok/tyzden16/Muz.java
@@ -3,26 +3,8 @@ package druhypolrok.tyzden16;
 public class Muz {
 
     private final String pohlavie = "Muz";
-    private String meno;
-    private int vek;
 
     public String getPohlavie() {
         return pohlavie;
-    }
-
-    public String getMeno() {
-        return meno;
-    }
-
-    public void setMeno(String meno) {
-        this.meno = meno;
-    }
-
-    public int getVek() {
-        return vek;
-    }
-
-    public void setVek(int vek) {
-        this.vek = vek;
     }
 }
--- src/main/java/druhypolrok/tyzden16/Zena.java
+++ src/main/java/druhypolrok/tyzden16/Zena.java
@@ -1,28 +1,14 @@
 package druhypolrok.tyzden16;
 
-public class Zena {
+public class Zena extends Entita{
 
     private final String pohlavie = "Zena";
-    private String meno;
-    private int vek;
+
+    public Zena(int vek, String meno, int vyska){
+        super(vek,meno,vyska);
+    }
 
     public String getPohlavie() {
         return pohlavie;
-    }
-
-    public String getMeno() {
-        return meno;
-    }
-
-    public void setMeno(String meno) {
-        this.meno = meno;
-    }
-
-    public int getVek() {
-        return vek;
-    }
-
-    public void setVek(int vek) {
-        this.vek = vek;
     }
 }
