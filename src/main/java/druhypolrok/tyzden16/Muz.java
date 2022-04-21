1f0afe5210207bd2b8683e1419896c33cb2a5ad1
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2022-04-21 09:54:40 +0200

    ss

--- src/main/java/druhypolrok/tyzden16/Muz.java
+++ src/main/java/druhypolrok/tyzden16/Muz.java
@@ -1,8 +1,12 @@
 package druhypolrok.tyzden16;
 
-public class Muz {
+public class Muz extends Entita{
 
     private final String pohlavie = "Muz";
+
+    public Muz(int vek, String meno, int vyska){
+        super(vek,meno,vyska);
+    }
 
     public String getPohlavie() {
         return pohlavie;
