5935674062955402f01f6fa1d1707877aeeca5bb
Kristian Palkovic <Kristian.Palkovic@sosholic.sk>
2022-06-09 11:09:29 +0200

    ss

---
+++ src/main/java/druhypolrok/tyzden23/Izba/Ceiling.java
@@ -0,0 +1,18 @@
+package druhypolrok.tyzden23.Izba;
+
+public class Ceiling {
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
